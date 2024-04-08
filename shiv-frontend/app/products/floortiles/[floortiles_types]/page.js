d2122ce5c250065948780f04929508c4eb7d49dc
nehashah4590 <ns457989@gmail.com>
2024-04-08 16:53:22 +0545

    o

--- shiv-frontend/app/products/floortiles/[floortiles_types]/page.js
+++ shiv-frontend/app/products/floortiles/[floortiles_types]/page.js
@@ -26,8 +26,9 @@ export default function page() {
   const room = sliced.charAt(0).toUpperCase() + sliced.slice(1);
   const tiles = "Floor Tiles";
 
-  const searchParams = useSearchParams();
-  const size = searchParams.get("value");
+  // const searchParams = useSearchParams();
+  // const size = searchParams.get("value");
+  const size = null;
   const url = size ? `${process.env.NEXT_PUBLIC_HOST}/tiles/photos/?product=${tiles}&size=${size}&room=${room}` : `${process.env.NEXT_PUBLIC_HOST}/tiles/photos/?product=${tiles}&size=${value}&room=${room}`;
 
 
